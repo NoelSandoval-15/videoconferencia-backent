0862a466604725d4b47b6ca283428ded57f7646c
NoelSandoval-15 <sandoval.erick@ficct.uagrm.edu.bo>
2026-05-26 21:00:34 -0400

    Subir cambios del proyecto y actualizar gitignore

--- apps/entrevistas/serializers.py
+++ apps/entrevistas/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from .models import Entrevista
 
+"este es el serializer de la app entrevistas"
 
 class EntrevistaSerializer(serializers.ModelSerializer):
     class Meta:
